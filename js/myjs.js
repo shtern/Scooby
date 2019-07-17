8f0da00388764d48dc5cc2e3cba2c339bbb22652
Deborah Sutton <sutton.deborah@gmail.com>
2019-07-17 15:25:03 +0300

    landing page

--- js/myjs.js
+++ js/myjs.js
@@ -1,1 +1,18 @@
-dd
+var scooby = {
+    landing: function () {
+        $("#buttonTutorial").on("click", this.instruct);
+        $("#buttonstart").on("click", this.start);
+    },
+    instruct: function () {
+        console.log("test");
+    },
+    start: function () {
+        console.log('test');
+        $(".landing").css("display", "none");
+        $(".contain").css("display", "flex");
+    }
+}
+
+scooby.landing();
+
+
