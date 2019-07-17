900f9f73a44bd64f35a6dab3ee8f7bddc774ae30
blyak008 <blyak008@umn.edu>
2019-07-17 14:22:16 +0300

    adding server with 15 min interval data collection

---
+++ complete/src/main/java/hello/HelloController.java
@@ -0,0 +1,82 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.scheduling.annotation.Scheduled;
+
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String index() throws Exception{
+        //URL url = new URL("https://vehicles.scootermap.com/api/vehicles?user_location=32.05282,34.7699463&northeast_point=32.086255, 34.777813&southwest_point=32.078731, 34.766921&company=bird&mode=charge&randomize=false");
+        //URL url = new URL("https://vehicles.scootermap.com/api/vehicles");
+        //URL url = new URL("https://api-prod.ibyke.io/v2/boards?latitude=32.05281297232161&longitude=34.77219253778458");
+        URL url = new URL("https://api-prod.ibyke.io/v2/boards?latitude=32.05281297232161&longitude=34.77219253778458");
+        HttpURLConnection con = (HttpsURLConnection) url.openConnection();
+        con.setRequestProperty("user-agent","ZenBike-New/3.21.0 (iPhone; iOS 12.3.1; Scale/3.00)");
+        con.setRequestProperty("x-lang", "en");
+        con.setRequestProperty("authentication","clientId=A34194B9-D430-4202-9544-D2460A920729;userId=ed2fe992-d851-4bdb-8721-2674991cc97a;ft=9e2b71c2d8d3520b8c29be0b069ad0c5");
+        con.setRequestProperty("x-package-name","com.zen.zbike");
+        con.setRequestProperty("x-app-version", "3.21.0");
+        con.setRequestProperty("x-country", "IL");
+        con.setRequestProperty("x-platform","iOS_12.3.1");
+        con.setRequestProperty("accept-language","en-IL;q=1");
+        con.setRequestProperty("accept","application/json");
+        con.setRequestProperty("content-type","application/json; charset=utf-8");
+        con.setRequestProperty("x-adv-id","3AFA6040-6B24-4018-BED6-4890EE01AF6C");
+        con.setRequestProperty("x-req-id","A34194B9-D430-4202-9544-D2460A920729");
+
+        con.setRequestMethod("GET");
+
+        int status = con.getResponseCode();
+//        File file = new File("data.txt");
+//
+//        // creates the file
+//        file.createNewFile();
+//        FileWriter writer = new FileWriter(file);
+//
+//        writer.append("1,2,3,4,5");
+//        writer.append("6,7,8,9,10");
+//        writer.close();
+        File file = new File("data15.txt");
+
+        // creates the file
+        //file.createNewFile();
+        System.out.println(file.getAbsolutePath());
+        // creates a FileWriter Object
+        FileWriter writer = new FileWriter(file);
+
+        // Writes the content to the file
+
+
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+
+        writer.append(content.toString());
+        writer.close();
+
+        return content.toString();
+    }
+
+
+    @Scheduled(cron= "* */15 * * * *")
+    private void timed() throws Exception{
+        System.out.println("here");
+        index();
+    }
+    
+}
